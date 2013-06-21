5af2c20b30a480270c90ea23bfdea6198aa27298
John Johansen <john.johansen@canonical.com>
2013-06-21 06:11:38 -0700

    UBUNTU: SAUCE: apparmor: 3.2 backport revert umode_t in mkdir 4572befe
    partial revert of 4572befe248fd0d94aedc98775e3f0ddc8a26651
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Dongjin Kim <dongjin.kim@agreeyamobility.net>

--- security/apparmor/lsm.c
+++ security/apparmor/lsm.c
@@ -295,7 +295,7 @@ static int apparmor_path_unlink(struct path *dir, struct dentry *dentry)
 }
 
 static int apparmor_path_mkdir(struct path *dir, struct dentry *dentry,
-			       umode_t mode)
+			       int mode)
 {
 	return common_perm_create(OP_MKDIR, dir, dentry, AA_MAY_CREATE,
 				  S_IFDIR);
